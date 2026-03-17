e406e64e39bbd7b8a6d9e8904dca99c8b6a5e7fd
NATAKRIT T. <127387856+tylerthecreaton@users.noreply.github.com>
2026-03-17 10:31:18 +0000

    add

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,17 +6,29 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+return !users.some((user) => user.username === username);
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+return users.some((user) => user.username === username && user.password === password);
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Error logging in" });
+  }
+
+  if (authenticatedUser(username, password)) {
+    const accessToken = jwt.sign({ data: password }, "access", { expiresIn: 60 * 60 });
+    req.session.authorization = { accessToken };
+    return res.status(200).json({ message: "User successfully logged in" });
+  }
+
+  return res.status(401).json({ message: "Invalid Login. Check username and password" });
 });
 
 // Add a book review
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,7 +7,19 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Unable to register user." });
+  }
+
+  const userExists = users.some((user) => user.username === username);
+  if (userExists) {
+    return res.status(409).json({ message: "User already exists!" });
+  }
+
+  users.push({ username, password });
+  return res.status(200).json({ message: "User successfully registered. Now you can login" });
 });
 
 // Get the book list available in the shop
@@ -62,7 +74,14 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  return res.status(200).json(book.reviews);
 });
 
 module.exports.general = public_users;
