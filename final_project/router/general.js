7df04d92bc52ac04c0199ff0c9391d3ea6f8d5a3
NATAKRIT T. <127387856+tylerthecreaton@users.noreply.github.com>
2026-03-17 10:24:32 +0000

    add

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -47,7 +47,16 @@ public_users.get('/author/:author',function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title.toLowerCase();
+  const matchedBooks = {};
+
+  Object.keys(books).forEach((isbn) => {
+    if (books[isbn].title.toLowerCase() === title) {
+      matchedBooks[isbn] = books[isbn];
+    }
+  });
+
+  return res.status(200).json(matchedBooks);
 });
 
 //  Get book review
